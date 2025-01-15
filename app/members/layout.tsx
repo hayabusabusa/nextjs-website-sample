9e9f010c2145b6cfa6f0711230df76cd1ae03005
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-15 19:59:55 +0900

    chapter4: ページごとにレイアウトを反映できる Nesting Layout の仕組みを利用
    `app` 配下のディレクトリごとに `layout.tsx` を作成すると、ページごとに異なるレイアウトを反映できる Nesting Layout の仕組みを利用。

---
+++ app/members/layout.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import styles from "./page.module.css";
+
+type Props = {
+    children: React.ReactNode;
+}
+
+export default function RootLayout({ children }: Props) {
+    return (
+        <>
+            <div className={styles.container}>{children}</div>
+        </>
+    );
+}
