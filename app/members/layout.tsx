48c43dd3c1156b55733fefba103c3ad807078b3f
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-15 22:00:22 +0900

    chapter4: レイアウトを共通化するためのコンポーネントを追加

--- app/members/layout.tsx
+++ app/members/layout.tsx
@@ -1,14 +1,11 @@
 import React from "react";
-import styles from "./page.module.css";
+
+import Sheet from "@/app/_components/Sheet";
 
 type Props = {
     children: React.ReactNode;
 }
 
 export default function RootLayout({ children }: Props) {
-    return (
-        <>
-            <div className={styles.container}>{children}</div>
-        </>
-    );
+    return <Sheet>{children}</Sheet>;
 }
