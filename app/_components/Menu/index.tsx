33041d6ed3d01538fab604e2e6c56ab5ea9260fb
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-16 01:45:40 +0900

    chapter4: メニュー部分の状態を更新する処理を追加

--- app/_components/Menu/index.tsx
+++ app/_components/Menu/index.tsx
@@ -1,13 +1,17 @@
 "use client";
 
+import cx from "classnames";
 import Link from "next/link";
 import Image from "next/image";
+import { useState } from "react";
 import styles from "./index.module.css";
 
 export default function Menu() {
+    const [isOpen, setOpen] = useState(false);
+
     return (
         <div>
-            <nav className={styles.nav}>
+            <nav className={cx(styles.nav, isOpen && styles.open)}>
                 <ul className={styles.items}>
                     <li>
                         <Link href="/news">ニュース</Link>
@@ -19,12 +23,22 @@ export default function Menu() {
                         <Link href="/contact">お問い合わせ</Link>
                     </li>
                 </ul>
+                <button 
+                    className={cx(styles.button, styles.close)}
+                    onClick={() => setOpen(false)}
+                >
+                    <Image
+                        src="/close.svg"
+                        alt="閉じる"
+                        width={24}
+                        height={24}
+                        priority
+                    />
+                </button>
             </nav>
             <button 
                 className={styles.button} 
-                onClick={() => {
-                    document.querySelector("nav")?.classList.add(styles.open);
-                }}
+                onClick={() => setOpen(true)}
             >
                 <Image
                     src="/menu.svg"
