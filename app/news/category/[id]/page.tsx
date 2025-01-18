32c2cf30bcf89283fe05cf8c0389edce4ba4818b
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-18 20:27:55 +0900

    chapter7: ニュースのカテゴリーページを実装

---
+++ app/news/category/[id]/page.tsx
@@ -0,0 +1,29 @@
+import { notFound } from "next/navigation";
+
+import { getCategoryDetail, getNewsList } from "@/app/_libs/microcms";
+import Category from "@/app/_components/Category";
+import NewsList from "@/app/_components/NewsList";
+
+type Props = {
+    params: {
+        id: string;
+    };
+};
+
+export default async function Page({ params }: Props) {
+    // 存在するカテゴリーかを確認.
+    const category = await getCategoryDetail(params.id).catch(notFound);
+    // microCMS では `filters` でコンテンツの絞り込みが可能.
+    const { contents: news } = await getNewsList({
+        filters: `category[equals]${category.id}`,
+    });
+
+    return (
+        <>
+            <p>
+                <Category category={category} /> の一覧
+            </p>
+            <NewsList news={news} />
+        </>
+    );
+}
