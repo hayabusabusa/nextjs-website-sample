87bec51d3d45c4659ce24ab05d1cb60a0bd91635
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-18 14:04:21 +0900

    chapter6: ニュース一覧画面のデータを MicroCMS から取得するように実装

---
+++ app/news/layout.tsx
@@ -0,0 +1,17 @@
+import React from "react";
+
+import Hero from "@/app/_components/Hero";
+import Sheet from "@/app/_components/Sheet";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function NewsLayout({ children }: Props) {
+    return (
+        <>
+            <Hero title="News" sub="ニュース" />
+            <Sheet>{children}</Sheet>
+        </>
+    );
+};
