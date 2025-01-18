6151c3f51c433b1eb14e8e8645f8a187c64e2382
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-18 13:41:58 +0900

    chapter5: トップページのニュース部分を MicroCMS から取得するように実装

--- app/_constants/index.ts
+++ app/_constants/index.ts
@@ -2,3 +2,8 @@
  * `/members` から取得するデータ件数の上限値.
  */
 export const MEMBERS_LIST_LIMIT = 100;
+
+/**
+ * トップページに表示するニュースの取得上限値.
+ */
+export const TOP_NEWS_LIMIT = 2;
--- app/page.tsx
+++ app/page.tsx
@@ -2,46 +2,14 @@ import Image from "next/image";
 import styles from "./page.module.css";
 
 import ButtonLink from "@/app/_components/ButtonLink";
+import { TOP_NEWS_LIMIT } from "@/app/_constants";
 import NewsList from "@/app/_components/NewsList";
-import { News } from "@/app/_libs/microcms";
+import { getNewsList } from "@/app/_libs/microcms";
 
-const data: {
-  contents: News[]
-} = {
-  contents: [
-    {
-      id: "1",
-      title: "渋谷にオフィスを移転しました",
-      category: {
-        name: "更新情報"
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19"
-    },
-    {
-      id: "2",
-      title: "当社 CEO が業界リーダー TOP 30 に選出されました",
-      category: {
-        name: "更新情報"
-      },
-      publishedAt: "2023/05/19",
-      createdAt: "2023/05/19"
-    },
-    {
-      id: "3",
-      title: "テストの記事です",
-      category: {
-        name: "更新情報"
-      },
-      publishedAt: "2023/04/19",
-      createdAt: "2023/04/19"
-    }
-  ]
-}
-
-export default function Home() {
-  // 表示件数は 2 件までに絞り込む.
-  const slicedData = data.contents.slice(0, 2);
+export default async function Home() {
+  const data = await getNewsList({
+    limit: TOP_NEWS_LIMIT,
+  });
 
   return (
     <>
@@ -62,7 +30,7 @@ export default function Home() {
       </section>
       <section className={styles.news}>
         <h2 className={styles.newsTitle}>News</h2>
-        <NewsList news={slicedData} />
+        <NewsList news={data.contents} />
         {/* レイアウトと余白に関してはコンポーネントに含めずに、親要素で指定する方が再利用性が高まる. */}
         <div className={styles.newsLink}>
           <ButtonLink href="/news">もっと見る</ButtonLink>
