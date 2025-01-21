be87cedb7a36e3b6ee6597746e7c405ae5a1d7e7
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-21 21:42:25 +0900

    chapter9: お問い合わせページを追加

---
+++ app/contact/layout.tsx
@@ -0,0 +1,19 @@
+import React from "react";
+
+import Hero from "@/app/_components/Hero";
+import Sheet from "@/app/_components/Sheet";
+
+type Props = {
+    children: React.ReactNode;
+};
+
+export default function RootLayout({ children }: Props) {
+    return (
+        <>
+            <Hero title="Contact" sub="お問い合わせ" />
+            <Sheet>
+                {children}
+            </Sheet>
+        </>
+    );
+}
