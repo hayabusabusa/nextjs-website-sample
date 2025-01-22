077e3b2d55521822576bcc494148a0e81135dd46
Shunya Yamada <hayabusabusaSY@gmail.com>
2025-01-22 20:08:46 +0900

    chapter10: Lighthouse で指摘されている LCP を修正するため背景画像を優先的に読み込むように修正
    Next.js の Image タグに `priority` を追加してトップページヘッダーの背景画像を優先的に読み込むように修正。

--- app/page.tsx
+++ app/page.tsx
@@ -28,6 +28,7 @@ export default async function Home() {
             alt=""
             width={4000}
             height={1200}
+            priority
           />
         </div>
       </section>
